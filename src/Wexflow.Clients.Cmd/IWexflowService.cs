7fda73a83f34abcc8f46488212024f75f9d9d0a6
aelassas <akram.elassas@gmail.com>
2017-01-29 12:12:30 +0000

    Updated Wexflow.Clients.Cmd

--- src/Wexflow.Clients.Cmd/IWexflowService.cs
+++ src/Wexflow.Clients.Cmd/IWexflowService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Wexflow.Core;
-using Wexflow.Core.Service.Contracts;
-using Nancy;
+﻿using Nancy;
 
 namespace Wexflow.Clients.Cmd
 {
