d4eb0a23530bd523dbd543040a651177846aa1cf
aelassas <akram.elassas@gmail.com>
2018-10-22 17:37:43 +0100

    Created cron unit test.

---
+++ tests/Wexflow.Tests/Workflow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wexflow.Tests
+{
+    [TestClass]
+    public class Workflow
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void WorkflowTest()
+        {
+            // TODO
+        }
+    }
+}
