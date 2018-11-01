ce952fb52830ee9bb6e0bc31664b1933ba662e6d
aelassas <akram.elassas@gmail.com>
2018-11-01 18:08:54 +0000

    Code refactoring.

--- src/Wexflow.Tasks.HttpDelete/HttpDelete.cs
+++ src/Wexflow.Tasks.HttpDelete/HttpDelete.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Security.Authentication;
 using System.Text;
