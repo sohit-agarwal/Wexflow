8ed28668b636b8f7c964e06e8391112518e54b8a
aelassas <akram.elassas@gmail.com>
2019-08-14 21:26:26 +0100

    Updated the style of password reset page in the backend.

--- src/backend/Wexflow.Backend/js/forgot-password.js
+++ src/backend/Wexflow.Backend/js/forgot-password.js
@@ -41,4 +41,17 @@
 
         });
     }
+
+    $(".field-wrapper .field-placeholder").on("click", function () {
+        $(this).closest(".field-wrapper").find("input").focus();
+    });
+    $(".field-wrapper input").on("keyup", function () {
+        var value = $.trim($(this).val());
+        if (value) {
+            $(this).closest(".field-wrapper").addClass("hasValue");
+        } else {
+            $(this).closest(".field-wrapper").removeClass("hasValue");
+        }
+    });
+
 }
