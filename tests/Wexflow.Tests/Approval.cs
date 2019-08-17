9cd3db0e464d24496bc995ce5c1202f37a4344c8
aelassas <akram.elassas@gmail.com>
2019-08-17 19:29:30 +0100

    Added Approval unit test

---
+++ tests/Wexflow.Tests/Approval.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wexflow.Tests
+{
+    [TestClass]
+    public class Approval
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void ApprovalTest()
+        {
+            // TODO
+            //Stopwatch stopwatch = Stopwatch.StartNew();
+            //Helper.StartWorkflow(125);
+            //stopwatch.Stop();
+            //Assert.IsTrue(stopwatch.ElapsedMilliseconds > 2000);
+        }
+    }
+}
