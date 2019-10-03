001a729fdca84b476f49d11e00ad925353243a5c
Fábio Naspolini <fabionaspolini@gmail.com>
2019-10-03 17:50:47 -0300

    Files joiner task. Task to joiner spliteds files. Can join N files within the same directory, the task identified by the initial part of the name.

---
+++ src/dotnet-core/Wexflow.Tasks.FilesJoiner/FilesJoiner.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml.Linq;
+using Wexflow.Core;
+
+namespace Wexflow.Tasks.FilesJoiner
+{
+    public class FilesJoiner : Task
+    {
+        public FilesJoiner(XElement xe, Workflow wf)
+            : base(xe, wf)
+        {
+        }
+
+        private int GetNumberPart(string path)
+        {
+            var lastIndex = path.LastIndexOf("_");
+            if (lastIndex == -1) return int.MaxValue;
+            var substring = path.Substring(lastIndex + 1, path.Length - lastIndex - 1);
+            return int.TryParse(substring, out var result) ? result : int.MaxValue;
+        }
+
+        public override TaskStatus Run()
+        {
+            Info("Concatenating files...");
+
+            bool success = true;
+            bool atLeastOneSucceed = false;
+
+            var files = SelectFiles().OrderBy(i => GetNumberPart(i.Path)).ToArray();
+
+            if (files.Length > 0)
+            {
+                var fileName = Path.GetFileName(files[0].FileName);
+                foreach (var file in files)
+                {
+                    if (file.FileName.EndsWith("_1"))
+                        fileName = file.FileName.Remove(fileName.Length - 2, 2);
+                }
+
+                var concatPath = Path.Combine(Workflow.WorkflowTempFolder, fileName);
+
+                if (File.Exists(concatPath))
+                {
+                    File.Delete(concatPath);
+                }
+
+                using (var output = File.Create(concatPath))
+                { 
+                    foreach (FileInf file in files)
+                    {
+                        Info("Joiner " + file.Path);
+                        try
+                        {
+                            using (var input = File.OpenRead(file.Path))
+                            {
+                                input.CopyTo(output);
+                            }
+
+                            if (!atLeastOneSucceed) atLeastOneSucceed = true;
+                        }
+                        catch (ThreadAbortException)
+                        {
+                            throw;
+                        }
+                        catch (Exception e)
+                        {
+                            ErrorFormat("An error occured while concatenating the file {0}", e, file.Path);
+                            success = false;
+                        }
+                    }
+                }
+
+                if (success)
+                {
+                    InfoFormat("Concatenation file generated: {0}", fileName);
+                }
+
+                Files.Add(new FileInf(concatPath, Id));
+            }
+
+            var status = Status.Success;
+
+            if (!success && atLeastOneSucceed)
+            {
+                status = Status.Warning;
+            }
+            else if (!success)
+            {
+                status = Status.Error;
+            }
+
+            Info("Task finished.");
+            return new TaskStatus(status, false);
+        }
+    }
+}
---
+++ tests/dotnet-core/Wexflow.DotnetCore.Tests/FilesJoiner.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wexflow.DotnetCore.Tests
+{
+    [TestClass]
+    public class FilesJoiner
+    {
+//        private static readonly string FilesSplitterFolder = @"C:\WexflowTesting\FilesSplitter\";
+        private static readonly string FilesSplitterFolder = @"C:\WexflowTesting\FilesSplitter\";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+//            Helper.DeleteFiles(FilesSplitterFolder);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+//            Helper.DeleteFiles(FilesSplitterFolder);
+        }
+
+        [TestMethod]
+        public void FilesSplitterTest()
+        {
+            /*string[] files = GetFiles();
+            Assert.AreEqual(0, files.Length);
+            Helper.StartWorkflow(57);
+            files = GetFiles();
+            Assert.AreEqual(510, files.Length);*/
+            Helper.StartWorkflow(1001);
+            Assert.AreEqual(true, true);
+        }
+
+        private string[] GetFiles()
+        {
+            return Directory.GetFiles(FilesSplitterFolder, "*_*");
+        }
+    }
+}
