256a3e00fde36d4118d5cce064c171765f050354
Fábio Naspolini <fabionaspolini@gmail.com>
2019-10-04 00:54:10 -0300

    Automatizing database build dotnet-core for LiteDB

--- src/dotnet-core/Wexflow.Scripts.LiteDB/Program.cs
+++ src/dotnet-core/Wexflow.Scripts.LiteDB/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 using Wexflow.Core.LiteDB;
 using Wexflow.Scripts.Core;
 
@@ -13,12 +14,17 @@ namespace Wexflow.Scripts.LiteDB
             {
                 IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.OSVersion.Platform}.json", optional: true, reloadOnChange: true)
                 .Build();
 
                 var workflowsFolder = config["workflowsFolder"];
                 Db db = new Db(config["connectionString"]);
 
                 Helper.InsertWorkflowsAndUser(db, workflowsFolder);
+
+                BuildDatabase("Windows", "windows");
+                BuildDatabase("Linux", "linux");
+                BuildDatabase("Mac OS X", "macos");
             }
             catch (Exception e)
             {
@@ -27,6 +33,24 @@ namespace Wexflow.Scripts.LiteDB
 
             Console.Write("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static void BuildDatabase(string info, string platformFolder)
+        {
+            Console.WriteLine($"=== Build {info} database ===");
+            var dbPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "..", "..",
+                "samples", "dotnet-core", platformFolder, "Wexflow", "Database", "Wexflow.db");
+
+            var workflowsFolder = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "..", "..",
+                "samples", "MongoDB", "dotnet-core", platformFolder);
+
+            if (!Directory.Exists(workflowsFolder)) throw new DirectoryNotFoundException("Invalid workflows folder: " + workflowsFolder);
+            if (File.Exists(dbPath)) File.Delete(dbPath);
+
+            var db = new Db(dbPath);
+            Helper.InsertWorkflowsAndUser(db, workflowsFolder);
         }
     }
 }
