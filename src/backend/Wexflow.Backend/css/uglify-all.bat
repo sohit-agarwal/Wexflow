254e35d5c0c472aa5c66c00f45bd44f6c82ef377
aelassas <akram.elassas@gmail.com>
2020-04-02 21:46:13 +0100

    Updated backend scripts

--- src/backend/Wexflow.Backend/css/uglify-all.bat
+++ src/backend/Wexflow.Backend/css/uglify-all.bat
@@ -3,6 +3,7 @@ call uglify-forgot-password.bat
 call uglify-dashboard.bat
 call uglify-manager.bat
 call uglify-approval.bat
+call uglify-editor.bat
 call uglify-designer.bat
 call uglify-history.bat
 call uglify-users.bat
