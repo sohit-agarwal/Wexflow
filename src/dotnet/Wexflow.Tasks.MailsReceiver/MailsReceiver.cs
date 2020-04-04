92350d5771b367144ce96595005f4396720776b6
aelassas <akram.elassas@gmail.com>
2020-04-04 11:20:53 +0100

    Added support for imap in MailsReceiver (#202)

--- src/dotnet/Wexflow.Tasks.MailsReceiver/MailsReceiver.cs
+++ src/dotnet/Wexflow.Tasks.MailsReceiver/MailsReceiver.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Net.Pop3;
+using MailKit.Search;
+using MimeKit;
+using System;
 using System.IO;
-using Wexflow.Core;
-using System.Xml.Linq;
+using System.Linq;
 using System.Threading;
-using OpenPop.Pop3;
+using System.Xml.Linq;
+using Wexflow.Core;
 
 namespace Wexflow.Tasks.MailsReceiver
 {
+    public enum Protocol
+    {
+        Pop3,
+        Imap
+    }
+
     public class MailsReceiver : Task
     {
         public string Host { get; private set; }
@@ -16,17 +27,18 @@ namespace Wexflow.Tasks.MailsReceiver
         public string Password { get; private set; }
         public int MessageCount { get; private set; }
         public bool DeleteMessages { get; private set; }
+        public Protocol Protocol { get; private set; }
 
-        public MailsReceiver(XElement xe, Workflow wf)
-            : base(xe, wf)
+        public MailsReceiver(XElement xe, Workflow wf) : base(xe, wf)
         {
             Host = GetSetting("host");
-            Port = int.Parse(GetSetting("port","993"));
+            Port = int.Parse(GetSetting("port", "993"));
             EnableSsl = bool.Parse(GetSetting("enableSsl", "true"));
             User = GetSetting("user");
             Password = GetSetting("password");
             MessageCount = int.Parse(GetSetting("messageCount"));
-            DeleteMessages = bool.Parse(GetSetting("deleteMessages","false"));
+            DeleteMessages = bool.Parse(GetSetting("deleteMessages", "false"));
+            Protocol = (Protocol)Enum.Parse(typeof(Protocol), GetSetting("protocol", "pop3"), true);
         }
 
         public override TaskStatus Run()
@@ -38,52 +50,115 @@ namespace Wexflow.Tasks.MailsReceiver
 
             try
             {
-                using (var client = new Pop3Client())
+                switch (Protocol)
                 {
-                    client.Connect(Host, Port, EnableSsl);
-                    client.Authenticate(User, Password);
-
-                    var count = client.GetMessageCount();
-
-                    // We want to download messages
-                    // Messages are numbered in the interval: [1, messageCount]
-                    // Ergo: message numbers are 1-based.
-                    // Most servers give the latest message the highest number
-                    for (int i = Math.Min(MessageCount, count); i > 0; i--)
-                    {
-                        var message = client.GetMessage(i);
-                        string messageFileName = "message_" + i + "_" + string.Format("{0:yyyy-MM-dd-HH-mm-ss-fff}", message.Headers.DateSent);
-                        string messagePath = Path.Combine(Workflow.WorkflowTempFolder, messageFileName + ".eml");
-                        File.WriteAllBytes(messagePath, message.RawMessage);
-                        Files.Add(new FileInf(messagePath, Id));
-                        InfoFormat("Message {0} received. Path: {1}", i, messagePath);
-
-                        // save attachments
-                        if (message.MessagePart.MessageParts != null)
+                    case Protocol.Imap:
+                        using (var client = new ImapClient())
                         {
-                            foreach (var messagePart in message.MessagePart.MessageParts)
+                            client.Connect(Host, Port, EnableSsl);
+                            client.Authenticate(User, Password);
+                            client.Inbox.Open(FolderAccess.ReadOnly);
+
+                            var uids = client.Inbox.Search(SearchQuery.All);
+
+                            var count = uids.Count();
+
+                            for (int i = Math.Min(MessageCount, count); i > 0; i--)
                             {
-                                if (messagePart.IsAttachment)
+                                var message = client.Inbox.GetMessage(uids[i]);
+                                string messageFileName = "message_" + i + "_" + string.Format("{0:yyyy-MM-dd-HH-mm-ss-fff}", message.Date);
+                                string messagePath = Path.Combine(Workflow.WorkflowTempFolder, messageFileName + ".eml");
+                                message.WriteTo(messagePath);
+                                Files.Add(new FileInf(messagePath, Id));
+                                InfoFormat("Message {0} received. Path: {1}", i, messagePath);
+
+                                // save attachments
+                                var j = 0;
+                                var attachments = message.Attachments.ToList();
+                                foreach (var attachment in attachments)
                                 {
-                                    string attachmentPath = Path.Combine(Workflow.WorkflowTempFolder, messageFileName + "_" + messagePart.FileName);
-                                    File.WriteAllBytes(attachmentPath, messagePart.Body);
-                                    Files.Add(new FileInf(attachmentPath, Id));
-                                    InfoFormat("Attachment {0} of mail {1} received. Path: {2}", messagePart.FileName, i, attachmentPath);
+                                    if (attachment.IsAttachment)
+                                    {
+                                        string attachmentPath = Path.Combine(Workflow.WorkflowTempFolder, messageFileName + "_" + (attachment is MessagePart ? ++j + ".eml" : ((MimePart)attachment).FileName));
+
+                                        if (attachment is MessagePart)
+                                        {
+                                            ((MessagePart)attachment).WriteTo(attachmentPath);
+                                        }
+                                        else
+                                        {
+                                            using (var stream = File.Create(attachmentPath))
+                                            {
+                                                ((MimePart)attachment).Content.DecodeTo(stream);
+                                            }
+                                        }
+
+                                        Files.Add(new FileInf(attachmentPath, Id));
+                                        InfoFormat("Attachment {0} of mail {1} received. Path: {2}", (attachment is MessagePart ? j + ".eml" : ((MimePart)attachment).FileName), i, attachmentPath);
+                                    }
+                                }
+
+
+                                if (!atLeastOneSucceed)
+                                {
+                                    atLeastOneSucceed = true;
                                 }
                             }
-                        }
 
-                        if (DeleteMessages)
-                        {
-                            client.DeleteMessage(i);
+                            client.Disconnect(true);
                         }
+                        break;
+                    case Protocol.Pop3:
+                        using (var client = new Pop3Client())
+                        {
+                            client.Connect(Host, Port, EnableSsl);
+                            client.AuthenticationMechanisms.Remove("XOAUTH2");
+                            client.Authenticate(User, Password);
 
-                        if (!atLeastOneSucceed)
-                        {
-                            atLeastOneSucceed = true;
+                            var count = client.GetMessageCount();
+
+                            // We want to download messages
+                            // Messages are numbered in the interval: [1, messageCount]
+                            // Ergo: message numbers are 1-based.
+                            // Most servers give the latest message the highest number
+                            for (int i = Math.Min(MessageCount, count); i > 0; i--)
+                            {
+                                var message = client.GetMessage(i);
+                                string messageFileName = "message_" + i + "_" + string.Format("{0:yyyy-MM-dd-HH-mm-ss-fff}", message.Date);
+                                string messagePath = Path.Combine(Workflow.WorkflowTempFolder, messageFileName + ".eml");
+                                message.WriteTo(messagePath);
+                                Files.Add(new FileInf(messagePath, Id));
+                                InfoFormat("Message {0} received. Path: {1}", i, messagePath);
+
+                                // save attachments
+                                var attachments = message.Attachments.ToList();
+                                foreach (var attachment in attachments)
+                                {
+                                    if (attachment.IsAttachment)
+                                    {
+                                        string attachmentPath = Path.Combine(Workflow.WorkflowTempFolder, messageFileName + "_" + attachment.ContentId);
+                                        attachment.WriteTo(attachmentPath);
+                                        Files.Add(new FileInf(attachmentPath, Id));
+                                        InfoFormat("Attachment {0} of mail {1} received. Path: {2}", attachment.ContentId, i, attachmentPath);
+                                    }
+                                }
+
+                                if (DeleteMessages)
+                                {
+                                    client.DeleteMessage(i);
+                                }
+
+                                if (!atLeastOneSucceed)
+                                {
+                                    atLeastOneSucceed = true;
+                                }
+                            }
+
+                            client.Disconnect(true);
                         }
-                    }
+                        break;
                 }
+
             }
             catch (ThreadAbortException)
             {
